6bcb174916a8a44cf1668ffa20b4ee2e33e525ef
Eren Guldas <erenguldas@gmail.com>
2022-08-05 06:48:38 +0300

    CU-2np39fj - user settings layout styling

--- src/components/layout/user-settings-layout/header/index.js
+++ src/components/layout/user-settings-layout/header/index.js
@@ -2,8 +2,8 @@ import { SearchInput } from '@/components/ui/inputs'
 
 const UserSettingsHeader = () => {
   return (
-    <div className="relative py-10">
-      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div className="relative pt-12 pb-6">
+      <div className="px-4 mx-auto max-w-7xl sm:px-0">
         <div className="flex items-center justify-between gap-4">
           <h1 className="text-3xl font-medium">Settings</h1>
           <SearchInput />
---
+++ src/components/layout/user-settings-layout/sidebar/index.js
@@ -0,0 +1,52 @@
+import { useRouter } from 'next/router'
+import { twMerge } from 'tailwind-merge'
+import NextLink from 'next/link'
+
+const links = [
+  { name: 'My profile', href: '/user-settings', current: false },
+  { name: 'Upload', href: '/user-settings/upload', current: false },
+  { name: 'Security', href: '/user-settings/security', current: false },
+  {
+    name: 'Notifications',
+    href: '/user-settings/notifications',
+    current: false,
+  },
+  { name: 'Wallet', href: '/user-settings/wallet', current: false },
+]
+
+const UserSettingsSidebar = () => {
+  const { pathname } = useRouter()
+  console.log(pathname)
+  return (
+    <aside className="py-6 lg:pr-12 lg:col-span-2">
+      <nav className="space-y-1">
+        {links.map((item) => (
+          <NextLink
+            href={{
+              pathname: item.href,
+            }}
+            scroll={false}
+            shallow={true}
+            key={`${item.name}-${Math.random() * 100}`}
+          >
+            <a
+              key={item.name}
+              href={item.href}
+              className={twMerge(
+                pathname.includes(item.href)
+                  ? 'bg-blue-50 border-blue-500 text-blue-700 hover:bg-blue-50 hover:text-teal-700'
+                  : 'border-transparent text-gray-500 hover:bg-gray-50 hover:text-gray-700',
+                'group border-l-4 px-3 py-2 flex items-center text-sm font-medium',
+              )}
+              aria-current={item.href === pathname ? 'page' : null}
+            >
+              <span className="truncate">{item.name}</span>
+            </a>
+          </NextLink>
+        ))}
+      </nav>
+    </aside>
+  )
+}
+
+export default UserSettingsSidebar
