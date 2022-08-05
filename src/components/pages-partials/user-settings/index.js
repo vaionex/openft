d33fc8480f32e963c6eb5cbd61ef6f42d2806ee5
Eren Guldas <erenguldas@gmail.com>
2022-08-05 11:05:25 +0300

    CU-2np39fj - user settings profile card

--- src/components/pages-partials/user-settings/index.js
+++ src/components/pages-partials/user-settings/index.js
@@ -1,7 +1,32 @@
 import UserSettingsLayout from '@/components/layout/user-settings-layout'
+import ProfileHeaderCard from '@/components/ui/cards/profile-header-card'
+
+const profile = {
+  name: 'Olivia Cooper',
+  title: 'UI Artist',
+  email: 'olivia.cooper@example.com',
+  avatar:
+    'https://images.unsplash.com/photo-1524502397800-2eeaad7c3fe5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+  backgroundImage:
+    'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+  fields: [
+    ['Phone', '(555) 123-4567'],
+    ['Email', 'ricardocooper@example.com'],
+    ['Title', 'Senior Front-End Developer'],
+    ['Team', 'Product Development'],
+    ['Location', 'San Francisco'],
+    ['Sits', 'Oasis, 4th floor'],
+    ['Salary', '$145,000'],
+    ['Birthday', 'June 8, 1990'],
+  ],
+}
 
 const UserSettingsMain = () => {
-  return <UserSettingsLayout>UserSettingsMain</UserSettingsLayout>
+  return (
+    <UserSettingsLayout>
+      <ProfileHeaderCard profile={profile} />
+    </UserSettingsLayout>
+  )
 }
 
 export default UserSettingsMain
