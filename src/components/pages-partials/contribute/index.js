f01f418d8eaedb05b1f540a5c526917c07ed10c0
Mert <m.sametatali@gmail.com>
2022-08-10 16:10:13 +0300

    CU-2np39pe - Contribute page

---
+++ src/components/pages-partials/contribute/index.js
@@ -0,0 +1,12 @@
+import SharedLayout from '@/components/layout/shared-layout'
+import Form from './form'
+
+const ContributePageMain = () => {
+  return (
+    <SharedLayout title="Contribute">
+      <Form />
+    </SharedLayout>
+  )
+}
+
+export default ContributePageMain
