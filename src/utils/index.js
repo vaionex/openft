246a799eafa9df5d50b1e2c53a6d30b6a61b6087
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2022-08-18 18:24:09 +0500

    remove unused function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,5 @@
 import { getFirstCharsOfName } from './getFirstCharsOfName'
-import { getUserFromCookie } from './userCookies'
-import { setUserCookie } from './userCookies'
-import { removeUserCookie } from './userCookies'
 
 export {
   getFirstCharsOfName,
-  getUserFromCookie,
-  setUserCookie,
-  removeUserCookie
 }
