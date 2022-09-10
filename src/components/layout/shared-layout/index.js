7b647daddcf3bd1f5ee496cef155813bce660d17
Eren Guldas <erenguldas@gmail.com>
2022-09-11 01:03:02 +0300

    removed page transitions

--- src/components/layout/shared-layout/index.js
+++ src/components/layout/shared-layout/index.js
@@ -3,36 +3,19 @@ import Head from 'next/head'
 import Footer from '../footer'
 import Header from '../header'
 import { twMerge } from 'tailwind-merge'
-import { motion } from 'framer-motion'
-import pageTransitionVariants from '@/animations/page-transition'
-import { useRouter } from 'next/router'
 
 const SharedLayout = ({ children, title, className }) => {
-  const router = useRouter()
-
-  const runPageTransition = () => {
-    return (
-      !router?.pathname?.includes('user-settings') && pageTransitionVariants
-    )
-  }
-
   return (
     <>
       <Head>
         <title>Openft | {title}</title>
       </Head>
-      <motion.div
-        variants={runPageTransition()}
-        initial="hidden"
-        animate="visible"
-        exit="exit"
-      >
-        <Header />
-        <main className={twMerge('min-h-[calc(100vh-(6rem+8rem))]', className)}>
-          {children}
-        </main>
-        <Footer />
-      </motion.div>
+
+      <Header />
+      <main className={twMerge('min-h-[calc(100vh-(6rem+8rem))]', className)}>
+        {children}
+      </main>
+      <Footer />
     </>
   )
 }
