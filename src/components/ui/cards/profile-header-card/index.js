794c15c723841a20951a194e46ecf88502d0af90
Eren Guldas <erenguldas@gmail.com>
2022-09-30 21:32:30 +0300

    cover image update, delete on settings

--- src/components/ui/cards/profile-header-card/index.js
+++ src/components/ui/cards/profile-header-card/index.js
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types'
 import { twMerge } from 'tailwind-merge'
 import { Avatar, AvatarWithName } from '@/components/ui/avatars'
 import ButtonWLoading from '@/components/ui/button-w-loading'
+import { PencilIcon, PlusCircleIcon, TrashIcon } from '@heroicons/react/outline'
+import { useSelector } from 'react-redux'
+import userSelector from '@/redux/selectors/user'
+import { useCallback, useState } from 'react'
+import { firebaseDeleteImage, firebaseUploadUserImage } from '@/firebase/utils'
+import ModalConfirm from '../../modal-confirm'
+import Spinner from '../../spinner'
+import { checkValidation } from '@/utils/imageValidation'
+import getFileExt from '@/utils/getFileExt'
+import ImageCropper from '../../image-cropper'
 
 const ProfileHeaderCard = ({
   user,
@@ -10,10 +20,81 @@ const ProfileHeaderCard = ({
   isSuccess,
   onSubmit,
   isError,
+  acceptableFileTypes = ['image/jpeg', 'image/png', 'image/webp'],
 }) => {
+  const { currentUser } = useSelector(userSelector)
+  const [isCropping, setIsCropping] = useState(false)
+  const [uploading, setUploading] = useState(false)
+  const [selectedImage, setSelectedImage] = useState(null)
+  const [errorMap, setErrorMap] = useState(null)
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
+
+  const IMAGE_SIZE_LIMIT = 4 * 1000 * 1000
+  const IMAGE_ASPECT = 191 / 48
+  const IMAGE_RESOLUTION_LIMIT = {
+    width: 3840,
+    height: 2160,
+  }
+
+  const validateImage = async (file) => {
+    return await checkValidation(
+      file,
+      ['PNG', 'JPEG', 'JPG', 'WEBP'],
+      IMAGE_SIZE_LIMIT,
+      IMAGE_RESOLUTION_LIMIT,
+    )
+  }
+
+  const setImageState = async ({ id, file, croppedBlobFile }) => {
+    setUploading(true)
+    try {
+      await firebaseUploadUserImage({
+        user: currentUser,
+        imageFile: croppedBlobFile,
+        imageType: id,
+      })
+      setUploading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleOnChange = useCallback(async (e) => {
+    const imageFile = e.target.files[0]
+
+    const errorObjects = await validateImage(imageFile)
+
+    if (!errorObjects) {
+      const createFileUrl = URL.createObjectURL(imageFile)
+      setSelectedImage({
+        src: createFileUrl,
+        name: imageFile.name,
+        ext: getFileExt(imageFile.name),
+      })
+      setIsCropping(true)
+    } else {
+      alert(errorObjects.message)
+    }
+  }, [])
+
+  const clearEventValue = (e) => {
+    e.target.value = ''
+  }
+
+  const handleOnDelete = async () => {
+    const uid = currentUser?.uid
+    try {
+      const img = await firebaseDeleteImage({ uid, imageType: 'coverImage' })
+      console.log(img)
+      setIsDeleteModalOpen(false)
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
   return (
     <div>
-      <div className="relative w-full h-32 overflow-hidden bg-blue-300 rounded-lg lg:h-60">
+      <div className="relative w-full h-32 overflow-hidden rounded-lg group bg-gradient-to-tr opacity-80 from-blue-600 to-blue-300 lg:h-60">
         {user?.coverImage && (
           <Image
             src={user?.coverImage}
@@ -22,27 +103,78 @@ const ProfileHeaderCard = ({
             objectFit="cover"
           />
         )}
+        {currentUser?.coverImage && (
+          <div className="absolute hidden gap-1 group-hover:flex top-2 right-2">
+            <div
+              className="flex items-center justify-center w-8 h-8 bg-white rounded-md shadow-sm cursor-pointer bg-opacity-30"
+              onClick={() => setIsDeleteModalOpen(true)}
+            >
+              <TrashIcon className="w-5 h-5 text-white" />
+            </div>
+
+            <label
+              className="flex items-center justify-center w-8 h-8 bg-white rounded-md shadow-sm cursor-pointer bg-opacity-30"
+              htmlFor="cover"
+            >
+              <PencilIcon className="w-5 h-5 text-white" />
+            </label>
+            <input
+              className="absolute hidden"
+              type="file"
+              id="cover"
+              accept={acceptableFileTypes}
+              onChange={(e) => handleOnChange(e)}
+              onClick={clearEventValue}
+              disabled={uploading}
+            />
+          </div>
+        )}
+        {!uploading && !currentUser?.coverImage && (
+          <>
+            <label
+              className="absolute inset-0 flex items-center justify-center bg-white rounded-md shadow-sm cursor-pointer bg-opacity-30"
+              htmlFor="cover"
+            >
+              <PlusCircleIcon className="w-12 h-12 text-white" />
+            </label>
+            <input
+              className="absolute hidden"
+              type="file"
+              id="cover"
+              accept={acceptableFileTypes}
+              onChange={(e) => handleOnChange(e)}
+              onClick={clearEventValue}
+              disabled={uploading}
+            />
+          </>
+        )}
+
+        {uploading && (
+          <div className="absolute inset-0 flex items-center justify-center bg-white rounded-md shadow-sm cursor-pointer bg-opacity-30">
+            <Spinner className="w-12 h-12 mr-0 text-white" />
+          </div>
+        )}
       </div>
-      <div className=" sm:px-6 lg:px-8">
-        <div className="-mt-12 sm:-mt-10 sm:flex sm:items-end sm:space-x-5">
+      <div className="sm:px-6 lg:px-8">
+        <div className="flex items-end -mt-12 sm:-mt-10 sm:space-x-5">
           {user ? (
             user.profileImage ? (
               <Avatar
-                className="w-24 h-24 bg-blue-700 sm:w-32 sm:h-32"
+                className="w-24 h-24 ml-4 bg-blue-700 sm:w-32 sm:h-32"
                 user={user}
               />
             ) : (
               <AvatarWithName
-                className="w-24 h-24 text-xl sm:w-32 sm:h-32"
+                className="w-24 h-24 ml-4 text-xl sm:w-32 sm:h-32"
                 name={user.name}
               />
             )
           ) : (
-            <AvatarWithName className="w-24 h-24 sm:w-32 sm:h-32 " />
+            <AvatarWithName className="w-24 h-24 ml-4 sm:w-32 sm:h-32 " />
           )}
 
-          <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
-            <div className="flex-1 min-w-0 mb-6 sm:mb-0 sm:hidden md:block">
+          <div className="flex items-center justify-end flex-1 mt-6 sm:min-w-0 sm:space-x-6 sm:pb-1">
+            <div className="flex-1 hidden min-w-0 mb-6 sm:mb-0 md:block">
               <h2 className="mb-1 text-2xl font-medium text-gray-900 truncate">
                 {user?.name}
               </h2>
@@ -65,13 +197,30 @@ const ProfileHeaderCard = ({
             </div>
           </div>
         </div>
-        <div className="flex-1 hidden min-w-0 mt-6 sm:block md:hidden">
+        <div className="flex-1 block min-w-0 mt-6 md:hidden">
           <h2 className="text-2xl font-medium text-gray-900 truncate">
             {user?.name}
           </h2>
           <h3 className="font-normal text-gray-500">{user?.title}</h3>
         </div>
       </div>
+      {isCropping && (
+        <ImageCropper
+          id="coverImage"
+          image={selectedImage}
+          aspect={IMAGE_ASPECT}
+          isCropping={isCropping}
+          setIsCropping={setIsCropping}
+          setToState={setImageState}
+        />
+      )}
+      <ModalConfirm
+        onClose={() => setIsDeleteModalOpen(false)}
+        onConfirm={handleOnDelete}
+        isOpen={isDeleteModalOpen}
+        title="Delete Cover Image"
+        text="Are you sure you want to delete cover image?"
+      />
     </div>
   )
 }
