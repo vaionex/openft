f0b410b0da9c0d5acf6cd586f8608c72ff786a6f
Mert <m.sametatali@gmail.com>
2022-10-14 16:00:36 +0300

    add user-overview social links

--- src/components/pages-partials/user-overview/header/index.js
+++ src/components/pages-partials/user-overview/header/index.js
@@ -95,34 +95,118 @@ export default function Header({ userInfo }) {
           alt=""
         />
         <div className="flex relative max-w-7xl mx-auto z-10 top-36 sm:top-52 sm:left-7 space-x-1.5 sm:space-x-6">
-          {social.map((item) => (
+          {userInfo?.socialLinks?.facebook && (
             <a
-              key={item.name}
-              href={item.href}
+              target="_blank"
+              rel="noreferrer noopener"
+              href={`https://facebook.com/${userInfo?.socialLinks?.facebook}`}
               className="text-gray-100 hover:text-gray-200"
             >
-              <span className="sr-only">{item.name}</span>
-              <item.icon className="h-5 w-5 sm:h-6 sm:w-6" aria-hidden="true" />
+              <span className="sr-only">Facebook</span>
+              <svg
+                fill="currentColor"
+                className="h-5 w-5 sm:h-6 sm:w-6"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </a>
-          ))}
+          )}
+          {userInfo?.socialLinks?.instagram && (
+            <a
+              href={`https://instagram.com/${userInfo?.socialLinks?.instagram}`}
+              rel="noreferrer noopener"
+              target="_blank"
+              className="text-gray-100 hover:text-gray-200"
+            >
+              <span className="sr-only">Instagram</span>
+              <svg
+                fill="currentColor"
+                className="h-5 w-5 sm:h-6 sm:w-6"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+          )}
+          {userInfo?.socialLinks?.website && (
+            <a
+              href={`https://${userInfo?.socialLinks?.website}`}
+              rel="noreferrer noopener"
+              target="_blank"
+              className="text-gray-100 hover:text-gray-200"
+            >
+              <span className="sr-only">Website</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={1.5}
+                className="h-5 w-5 sm:h-6 sm:w-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+                />
+              </svg>
+            </a>
+          )}
         </div>
       </div>
       <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
         <div className="-mt-12 sm:-mt-16 flex justify-center sm:space-x-5">
           <div className="flex z-20 bg-blue-500 relative h-20 w-20 rounded-xl ring-4 ring-white sm:h-32 sm:w-32">
-            <Image
-              className="rounded-xl"
-              objectFit="cover"
-              layout="fill"
-              src={userInfo?.profileImage ?? profile.avatar}
-              alt=""
-            />
+            {userInfo ? (
+              <Image
+                className="rounded-xl"
+                objectFit="cover"
+                layout="fill"
+                src={userInfo.profileImage}
+                alt=""
+              />
+            ) : (
+              <div
+                className="w-full h-full flex justify-center items-center"
+                role="status"
+              >
+                <svg
+                  aria-hidden="true"
+                  class="mr-2 w-16 h-16 text-gray-200 animate-spin fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span class="sr-only">Loading...</span>
+              </div>
+            )}
           </div>
         </div>
         <div className="mt-6 min-w-0 flex-1 flex justify-center flex-col items-center">
           <div>3d illustrator</div>
           <h2 className="truncate text-2xl py-2 font-bold text-gray-900">
-            {userInfo?.name}
+            {userInfo ? userInfo.name : '...'}
           </h2>
           <p className="max-w-3xl">
             Molestias repudiandae magni eos qui voluptatem aut. Non soluta
