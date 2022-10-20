03c931c54e084c58aa7d057f98d145dd9b7fd75b
Hasnain Shafqat <hasnainshafqatmlt@gmail.com>
2022-10-20 18:45:37 +0500

    Transactions history updated

---
+++ src/components/common/icons/externallink-icon.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+const SvgExternalLinkIcon = (props) => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M8.33398 4.99998H5.00065C4.08018 4.99998 3.33398 5.74617 3.33398 6.66665V15C3.33398 15.9205 4.08018 16.6666 5.00065 16.6666H13.334C14.2545 16.6666 15.0007 15.9205 15.0007 15V11.6666M11.6673 3.33331H16.6673M16.6673 3.33331V8.33331M16.6673 3.33331L8.33398 11.6666"
+      stroke="#667085"
+      stroke-width="1.6"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+)
+
+export default SvgExternalLinkIcon
