9f587a4fb6e604dc73df9b16912a6fcf6ac83366
Hasnain Shafqat <hasnainshafqatmlt@gmail.com>
2022-10-26 19:41:04 +0500

    setting page search updated

--- src/components/layout/user-settings-layout/header/index.js
+++ src/components/layout/user-settings-layout/header/index.js
@@ -1,18 +1,25 @@
 import { SearchInput } from '@/components/ui/inputs'
 import { InputMain } from '@/components/ui/inputs'
 import { MagnifyGlassIcon } from '@/components/common/icons'
+import { useRouter } from 'next/router'
+
 const UserSettingsHeader = () => {
+  const router = useRouter()
+
+  const keyDownHandler = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      router.push(`/discover?query=${event.target.value}`)
+    }
+  }
   return (
     <div className="relative pt-12 pb-6">
       <div className="flex flex-row justify-between px-4 mx-auto max-w-7xl sm:px-0">
         <div className="flex items-center justify-between gap-4">
           <h1 className="text-3xl font-medium">Settings</h1>
         </div>
-        {/* <div>
-          <form
-            onSubmit={(e) => handleSubmit(e, searchState)}
-            className="flex items-center w-full gap-2"
-          >
+        <div>
+          <form className="flex items-center w-full gap-2">
             <InputMain className="relative w-full pb-0 border-none">
               <span className="absolute inset-y-0 left-0 z-10 flex items-center pl-3 pointer-events-none">
                 <MagnifyGlassIcon className="w-5 h-5 text-gray-400 " />
@@ -27,13 +34,12 @@ const UserSettingsHeader = () => {
                 name="search"
                 id="search"
                 placeholder="Search"
-                //value={searchState}
                 inputClassName="pl-10 min-h-[44px]"
-                //onChange={handleChange}
+                onKeyDown={(e) => keyDownHandler(e)}
               />
             </InputMain>
           </form>
-        </div> */}
+        </div>
       </div>
     </div>
   )
