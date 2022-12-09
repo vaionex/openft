4adf556105e396eeef29b377131d8144a794250d
Hasnain Shafqat <hasnain@vaionex.com>
2022-12-09 11:04:59 +0500

    bug fixes

--- src/components/ui/forms/settings-forms/generate-form/dummy.js
+++ src/components/ui/forms/settings-forms/generate-form/dummy.js
@@ -6,7 +6,7 @@ export const DummyTwo = ({ data }) => {
       <div className="mx-auto">
         <div className="flex flex-wrap">
           {data?.map((ele) => (
-            <div key={ele.id} class="group relative p-px w-1/2 md:w-1/4">
+            <div key={ele.id} className="group relative p-px w-1/2 md:w-1/4">
               <img
                 alt="gallery"
                 className="block object-cover object-center w-full h-full"
@@ -37,11 +37,11 @@ export const DummyTwo = ({ data }) => {
 
 export const DummyOne = () => {
   return (
-    <section class="overflow-hidden text-gray-700">
-      <div class="mx-auto mt-6">
-        <div class="flex flex-wrap">
-          <div class="flex flex-wrap w-1/2">
-            <div class="w-full group relative">
+    <section className="overflow-hidden text-gray-700">
+      <div className="mx-auto mt-6">
+        <div className="flex flex-wrap">
+          <div className="flex flex-wrap w-1/2">
+            <div className="w-full group relative">
               <img
                 alt="gallery"
                 className="block object-cover object-center w-full h-full"
@@ -58,11 +58,11 @@ export const DummyOne = () => {
               </div>
             </div>
           </div>
-          <div class="flex flex-wrap w-1/2">
-            <div class=" group relative p-px w-1/2">
+          <div className="flex flex-wrap w-1/2">
+            <div className=" group relative p-px w-1/2">
               <img
                 alt="gallery"
-                class="block object-cover object-center w-full h-full"
+                className="block object-cover object-center w-full h-full"
                 src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
               />
               <div className="absolute p-[28px] top-0 left-0 w-full flex flex-col justify-between  bg-white/90 opacity-0 group-hover:h-full group-hover:opacity-100">
@@ -75,7 +75,7 @@ export const DummyOne = () => {
                 </a>
               </div>
             </div>
-            <div class="w-1/2 p-px group relative">
+            <div className="w-1/2 p-px group relative">
               <img
                 alt="gallery"
                 className="block object-cover object-center w-full h-full"
@@ -91,10 +91,10 @@ export const DummyOne = () => {
                 </a>
               </div>
             </div>
-            <div class="w-1/2 p-px group relative">
+            <div className="w-1/2 p-px group relative">
               <img
                 alt="gallery"
-                class="block object-cover object-center w-full h-full"
+                className="block object-cover object-center w-full h-full"
                 src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
               />
               <div className="absolute p-[28px] top-0 left-0 w-full flex flex-col justify-between  bg-white/90 opacity-0 group-hover:h-full group-hover:opacity-100">
@@ -107,10 +107,10 @@ export const DummyOne = () => {
                 </a>
               </div>
             </div>
-            <div class="w-1/2 p-px group relative">
+            <div className="w-1/2 p-px group relative">
               <img
                 alt="gallery"
-                class="block object-cover object-center w-full h-full"
+                className="block object-cover object-center w-full h-full"
                 src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(68).webp"
               />
               <div className="absolute p-[28px] top-0 left-0 w-full flex flex-col justify-between  bg-white/90 opacity-0 group-hover:h-full group-hover:opacity-100">
