3f3a9d9069cb38706a5044385ad4c753e667f51d
Kohze <robin@kohze.com>
2022-12-17 17:11:12 +0000

    fixed quotes

--- src/components/pages-partials/privacy/index.js
+++ src/components/pages-partials/privacy/index.js
@@ -26,7 +26,10 @@ const PrivacyMain = () => {
           <div className="px-4 py-16 sm:px-6 lg:px-8 sm:py-24">
             <div>
               <p className="max-w-2xl tracking-wide text-gray-500">
-              Our Privacy Policy explains how we collect, use, and share information about you when you use our websites, mobile apps, and other online products and services (collectively, the "Services") or when you otherwise interact with us.
+                Our Privacy Policy explains how we collect, use, and share
+                information about you when you use our websites, mobile apps,
+                and other online products and services (collectively, the
+                  &quot;Services&quot;) or when you otherwise interact with us.
               </p>
             </div>
             <div className="mt-12">
@@ -34,16 +37,33 @@ const PrivacyMain = () => {
                 What information do we collect?
               </h3>
               <p className="max-w-2xl tracking-wide text-gray-500">
-              When you use our Services, we may collect certain information about your device and your use of the Services. This includes information such as your IP address, device type, unique device identification numbers, browser type, broad geographic location (e.g. country or city-level location) and other technical information.
+                When you use our Services, we may collect certain information
+                about your device and your use of the Services. This includes
+                information such as your IP address, device type, unique device
+                identification numbers, browser type, broad geographic location
+                (e.g. country or city-level location) and other technical
+                information.
               </p>
               <p className="max-w-2xl mt-6 tracking-wide text-gray-500">
-              We may also collect information about how you use the Services, including the actions you take and the features you use.
+                We may also collect information about how you use the Services,
+                including the actions you take and the features you use.
               </p>
               <p className="max-w-2xl mt-6 tracking-wide text-gray-500">
-              We use cookies and similar technologies (e.g. web beacons, pixel tags, and clear GIFs) to collect information about your use of the Services. A cookie is a small data file that is transferred to your device. We use cookies to store your preferences and settings, to authenticate you when you log in, and to personalize your experience. You can control how cookies are used through your browser settings.
+                We use cookies and similar technologies (e.g. web beacons, pixel
+                tags, and clear GIFs) to collect information about your use of
+                the Services. A cookie is a small data file that is transferred
+                to your device. We use cookies to store your preferences and
+                settings, to authenticate you when you log in, and to
+                personalize your experience. You can control how cookies are
+                used through your browser settings.
               </p>
               <p className="max-w-2xl mt-6 tracking-wide text-gray-500">
-              We use Google Analytics and Firebase to collect and analyze data about how you use our Services. These tools use cookies and other tracking technologies to collect and store information such as the pages you visit, the time you spend on each page, and the links you click. We use this information to improve the performance and functionality of our Services.
+                We use Google Analytics and Firebase to collect and analyze data
+                about how you use our Services. These tools use cookies and
+                other tracking technologies to collect and store information
+                such as the pages you visit, the time you spend on each page,
+                and the links you click. We use this information to improve the
+                performance and functionality of our Services.
               </p>
             </div>
 
@@ -52,42 +72,58 @@ const PrivacyMain = () => {
                 How do we use your information?
               </h3>
               <p className="max-w-2xl tracking-wide text-gray-500">
-              We use the information we collect about you to provide, maintain, and improve the Services, to develop new features and services, and to protect us and our users.
+                We use the information we collect about you to provide,
+                maintain, and improve the Services, to develop new features and
+                services, and to protect us and our users.
               </p>
               <p className="max-w-2xl mt-6 tracking-wide text-gray-500">
-              We may also use the information for research and analytics purposes, to personalize your experience, and to communicate with you about products, services, and events.
+                We may also use the information for research and analytics
+                purposes, to personalize your experience, and to communicate
+                with you about products, services, and events.
               </p>
               <p className="max-w-2xl mt-6 tracking-wide text-gray-500">
-              We may share your information with third parties in certain circumstances, such as to comply with legal obligations, to protect the rights and safety of our users and third parties, and to enforce our policies.
+                We may share your information with third parties in certain
+                circumstances, such as to comply with legal obligations, to
+                protect the rights and safety of our users and third parties,
+                and to enforce our policies.
               </p>
             </div>
 
-           
             <div className="mt-12">
               <h4 className="mb-6 text-xl font-semibold">
                 How do we keep your information safe?
               </h4>
               <p className="max-w-2xl mt-6 tracking-wide text-gray-500">
-              We limit access to your information to only those employees, contractors, and service providers who need it to perform their duties.
+                We limit access to your information to only those employees,
+                contractors, and service providers who need it to perform their
+                duties.
               </p>
               <p className="max-w-2xl mt-6 tracking-wide text-gray-500">
-              We require third parties with whom we share your information to have appropriate security measures in place to protect that information.
+                We require third parties with whom we share your information to
+                have appropriate security measures in place to protect that
+                information.
               </p>
               <p className="max-w-2xl mt-6 tracking-wide text-gray-500">
-              We have implemented measures to detect and prevent data breaches, and we will promptly notify you if we discover a breach that affects your information.
+                We have implemented measures to detect and prevent data
+                breaches, and we will promptly notify you if we discover a
+                breach that affects your information.
               </p>
               <p className="max-w-2xl mt-6 tracking-wide text-gray-500">
-              We take your privacy seriously and are committed to protecting your information. If you have any questions or concerns about our privacy practices, please don't hesitate to contact us at admin@vaionex.com.
+                We take your privacy seriously and are committed to protecting
+                your information. If you have any questions or concerns about
+                our privacy practices, please don't hesitate to contact us at
+                admin@vaionex.com.
               </p>
-              
             </div>
-            
+
             <div className="mt-12">
               <h4 className="mb-6 text-xl font-semibold">
                 How can you contact us about this policy?
               </h4>
               <p className="max-w-2xl tracking-wide text-gray-500">
-                You can reach us at admin@vaionex.com if you have any questions about this policy. Vaionex Corporation, 2035 Sunset Lake Road, Suite B-2, Newark, DE 19702
+                You can reach us at admin@vaionex.com if you have any questions
+                about this policy. Vaionex Corporation, 2035 Sunset Lake Road,
+                Suite B-2, Newark, DE 19702
               </p>
             </div>
           </div>
