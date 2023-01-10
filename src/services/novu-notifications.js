92259d96957723a91abfeb5c3930bcebfb3130b4
Hasnain Shafqat <113719851+hasnainvaionex@users.noreply.github.com>
2023-01-10 17:49:47 +0500

    Cu 85zrhf745 (#142)
    * novu improvements
    
    * notification in paymail case fix

--- src/services/novu-notifications.js
+++ src/services/novu-notifications.js
@@ -1,41 +1,4 @@
-import { Novu } from '@novu/node'
 import fetch from 'node-fetch'
-
-export const CreateNovuNotification = async (userId, message) => {
-  const novu = new Novu(process.env.NEXT_PUBLIC_NOVU_API_KEY)
-
-  await novu
-    .trigger('in-app', {
-      to: {
-        subscriberId: userId,
-      },
-      payload: {
-        notificationContent: message,
-      },
-    })
-    .then((res) => res)
-    .catch((err) => {
-      console.log(err)
-    })
-}
-
-export const CreateNovuSubscriber = async (
-  userId,
-  userEmail,
-  userFirstName,
-) => {
-  const novu = new Novu(process.env.NEXT_PUBLIC_NOVU_API_KEY)
-
-  await novu.subscribers
-    .identify(userId, {
-      email: userEmail,
-      firstName: userFirstName,
-    })
-    .then((res) => res)
-    .catch((err) => {
-      console.log(err)
-    })
-}
 
 export async function SendNotification(userId, message) {
   try {
@@ -53,5 +16,25 @@ export async function SendNotification(userId, message) {
     const data = await sendNotification.json()
   } catch (err) {
     console.error('2::', err)
+  }
+}
+
+export async function CreateNovuSubscriber(userId, userEmail, userName) {
+  try {
+    const CreateNovuSubscriber = await fetch('/api/createNovuSub', {
+      method: 'POST',
+      body: JSON.stringify({
+        userId,
+        userEmail,
+        userName,
+      }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await CreateNovuSubscriber.json()
+  } catch (err) {
+    console.error('error::', err)
   }
 }
