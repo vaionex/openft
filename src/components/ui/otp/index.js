c5f86c2818013f22f9aa141f2faf4d311f54d3ae
mert <mert@vaionex.com>
2023-03-20 13:47:46 +0300

    update mfa popup zindex

--- src/components/ui/otp/index.js
+++ src/components/ui/otp/index.js
@@ -24,7 +24,11 @@ export default function OtpModal({
   return (
     <>
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-[999999]" onClose={closeModal}>
+        <Dialog
+          as="div"
+          className="relative z-[9999999999999]"
+          onClose={closeModal}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
