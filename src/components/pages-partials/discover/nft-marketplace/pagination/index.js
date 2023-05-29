334859b104269515b77515cc413572c25580ad75
meetmoradiya <gaminghawkeye14@gmail.com>
2023-05-29 22:42:35 +0530

    added url changes in the nft image

--- src/components/pages-partials/discover/nft-marketplace/pagination/index.js
+++ src/components/pages-partials/discover/nft-marketplace/pagination/index.js
@@ -13,16 +13,37 @@ const NFTMarketplacePagination = ({
     dispatch(setCurrentPage(data.selected + 1))
     toTopRef.current.scrollIntoView({ behavior: 'smooth' })
   }
-  const {  totalPage } = useSelector(nftSelector)
+  const { totalPage } = useSelector(nftSelector)
 
   return (
-    <div className="flex items-center justify-center">
-      <button
-        type="button"
-        className="bg-azul rounded-lg border border-azul text-azul py-2.5 px-2 flex w-40 justify-center items-center font-semibold"
-      >
-        <span className="text-white">Load More</span>
-      </button>
+    <div className="pagination-wrapper">
+      <ReactPaginate
+        previousLabel={
+          <span className="flex gap-1 text-mist">
+            <LeftIcon
+              className="w-5 h-5 hover:text-gray-500"
+              aria-hidden="true"
+            />
+            <span className="hidden sm:block">Previous</span>
+          </span>
+        }
+        nextLabel={
+          <span className="flex gap-1 text-mist">
+            <span className="hidden sm:block">Next</span>
+            <RightIcon
+              className="w-5 h-5 hover:text-gray-500"
+              aria-hidden="true"
+            />
+          </span>
+        }
+        pageCount={totalPage}
+        onPageChange={handlePageClick}
+        breakLabel={'...'}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={0}
+        containerClassName={'pagination'}
+        activeClassName={'active'}
+      />
     </div>
   )
 }
