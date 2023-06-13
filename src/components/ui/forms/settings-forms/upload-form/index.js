377888b9254b2195de0ef2f590a0bd7ca523916b
MHamxay <hamza@vaionex.com>
2023-06-13 18:54:49 +0500

    Update index.js

--- src/components/ui/forms/settings-forms/upload-form/index.js
+++ src/components/ui/forms/settings-forms/upload-form/index.js
@@ -184,7 +184,7 @@ const UploadForm = () => {
       const nftDataToFirebase = {
         ...formData,
         amountInBSV: amountInBSV,
-        imageURL: url,
+        imageURL: envMODE === 'DEV' ? nftImageForDisplay : url,
         ownerId: currentUser.uid,
         minterId: currentUser.uid,
         likes: 0,
